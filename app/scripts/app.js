9e5bd7474fb42781842aaf09f8b0a74d9a6e6e70
tmtrbocanegra <rabp_91@hotmail.com>
2017-09-11 16:49:26 -0500

    Iniciando

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name transnvAdminFrontendApp
+ * @description
+ * # transnvAdminFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('transnvAdminFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transnvAdminFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the transnvAdminFrontendApp
+ */
+angular.module('transnvAdminFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
