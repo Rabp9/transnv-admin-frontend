d29c2c0bed6159662e465bc2f52f88f838f30f5d
tmtrbocanegra <rabp_91@hotmail.com>
2017-10-05 16:44:37 -0500

    Actualizando

--- app/scripts/controllers/users-login.js
+++ app/scripts/controllers/users-login.js
@@ -8,6 +8,32 @@
  * Controller of the transnvAdminFrontendApp
  */
 angular.module('transnvAdminFrontendApp')
-.controller('UsersLoginCtrl', function ($scope) {
+.controller('UsersLoginCtrl', function ($scope, UsersService, $cookies, $location, $rootScope) {
+    
+    $scope.loginUser = function(user, boton) {
+        $('#' + boton).text('Login...');
+        $('#' + boton).addClass('disabled');
+        $('#' + boton).prop('disabled', true);
+        
+        UsersService.login(user, function(data) {
+            if (!data.user) {
+                $scope.message = data.message;
+            } else {
+                $cookies.putObject('transnv-user', data.user);
+                $cookies.put('transnv-token', data.token);
+                $rootScope.user = data.user;
+                $('#wrapper').removeClass('inLogin');
+                $location.path('/');
+            }
+        }, function(data) {
+            $('#' + boton).removeClass('disabled');
+            $('#' + boton).prop('disabled', false);
+            $('#' + boton).text('Login');
+            $scope.message =  {
+                type: 'error',
+                text: 'Nombre de usuario o contraseña incorrecta'
+            };
+        });
+    };
     
 });
