d23de733c8f5d4ae5bf43c42e527e503479540be
tmtrbocanegra <rabp_91@hotmail.com>
2018-02-22 10:49:39 -0500

    Avanzando

--- app/scripts/controllers/infos.js
+++ app/scripts/controllers/infos.js
@@ -11,8 +11,10 @@ angular.module('transnvAdminFrontendApp')
 .controller('InfosCtrl', function ($scope, infosservice, $uibModal, $utilsviewservice) {
     var search = ['quienes_somos_mensaje', 'historia_mensaje', 'directorio_mensaje',
     'ubicacion_mensaje', 'telefono', 'email', 'facebook_link', 'transnv_resumen', 
-    'ubicacion_codigo', 'enlace_1_titulo', 'enlace_2_titulo', 'enlace_3_titulo',
-    'enlace_1_link', 'enlace_2_link', 'enlace_3_link', 'twitter_link', 'direccion', 'copyright', 'ubicacion_lat_long'];
+    'enlace_1_titulo', 'enlace_2_titulo', 'enlace_3_titulo', 'enlace_1_link',
+    'enlace_2_link', 'enlace_3_link', 'twitter_link', 'direccion', 'copyright',
+    'quienes_somos', 'mision', 'vision', 'historia', 'ubicacion_lat_long', 
+    'nuestra_flota_mensaje', 'nuestro_personal_mensaje'];
     
     $scope.getInfos = function() {
         $scope.loading = true;
