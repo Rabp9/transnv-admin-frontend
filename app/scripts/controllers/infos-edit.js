e2307be52112a40ee16890d382154d9aaae724b4
tmtrbocanegra <rabp_91@hotmail.com>
2018-02-28 15:25:53 -0500

    Primer commit

--- app/scripts/controllers/infos-edit.js
+++ app/scripts/controllers/infos-edit.js
@@ -8,9 +8,23 @@
  * Controller of the transnvAdminFrontendApp
  */
 angular.module('transnvAdminFrontendApp')
-.controller('InfosEditCtrl', function ($scope, info, $uibModalInstance, infosservice, $utilsviewservice) {
+.controller('InfosEditCtrl', function ($scope, info, $uibModalInstance, infosservice, $utilsviewservice,
+    $rootScope) {
     $scope.info = $.extend(true, {}, info);
 
+    $scope.tmp_path_pages = $rootScope.path_location + 'img' + '/infos/'; 
+    $scope.tinymcePagesOptions = {
+        toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table | fontsizeselect | fontselect ",
+        fontsize_formats: "8pt 10pt 11pt 12pt 13pt 14pt 15pt 16pt 17pt 18pt 19pt 20pt 21pt 22pt 23pt 24pt 25pt 26pt 27pt 28pt",
+        plugins: 'lists autolink textcolor colorpicker link media preview table code image',
+        language_url : '/scripts/langs_tinymce/es.js',
+        file_browser_callback_types: 'image',
+        file_browser_callback: function(field_name, url, type, win) {
+            $scope.input = field_name;
+            $('#flImagen').click();
+        }
+    };
+    
     $scope.cancel = function() {
         $uibModalInstance.dismiss('cancel');
     };
@@ -27,4 +41,15 @@ angular.module('transnvAdminFrontendApp')
             $uibModalInstance.close(error.data);
         });
     };
+    
+    $scope.upload = function(image, errFiles) {
+        var fd = new FormData();
+        fd.append('file', image);
+        
+        infosservice.upload(fd, function(data) {
+            $scope.url = $scope.tmp_path_pages + data.filename;
+            document.getElementById($scope.input).value = $scope.url;
+        });
+    };
+    
 });
